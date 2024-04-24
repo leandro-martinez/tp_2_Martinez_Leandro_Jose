507b16e9d0aabe0d99d5548909dc20be6dbe9a0b
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-24 14:05:22 -0300

    creacion de la clase Producto y enums: OrigenFabricacion y Categoria

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -1,5 +1,81 @@
 package ar.edu.unju.fi.ejercicio1.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private Double precioUnitario;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+	
+	public enum OrigenFabricacion {
+		ARGENTINA,
+		CHINA,
+		BRASIL,
+		URUGUAY;
+	}
+	public enum Categoria {
+		TELEFONIA,
+		INFORMATICA,
+		ELECTROHOGAR,
+		HERRAMIENTAS;
+	}
+	
+	public Producto () {}
 
+	public Producto(String codigo, String descripcion, Double precioUnitario, OrigenFabricacion origenFabricacion,
+			Categoria categoria) {
+		super();
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origenFabricacion = origenFabricacion;
+		this.categoria = categoria;
+	}
+	
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precioUnitario=" + precioUnitario
+				+ ", origenFabricacion=" + origenFabricacion + ", categoria=" + categoria + "]";
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(Double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
 }
