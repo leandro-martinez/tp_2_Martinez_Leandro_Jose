2f8539e05a4d7a730204b3084bff9b5f0c43a6a0
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-25 17:21:11 -0300

    funcion crearJugador y validarPosicion creadas

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/main/Main.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/main/Main.java
@@ -1,12 +1,20 @@
 package ar.edu.unju.fi.ejercicio4.main;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
+import ar.edu.unju.fi.ejercicio4.model.Jugador;
 
 public class Main {
 	private static Scanner scanner;
+	private static List<Jugador> jugadores;
 	
 	public static void main(String[] args) {
+		jugadores = new ArrayList<>();
 		scanner = new Scanner(System.in);
 		int opcion = 0;
 		try {
@@ -23,7 +31,7 @@ public class Main {
 				scanner.nextLine();
 				
 				switch (opcion) {
-					case 1: System.out.println("Opcion 1");
+					case 1: crearJugador();
 						break;
 					case 2: System.out.println("Opcion 2");
 						break;
@@ -41,5 +49,67 @@ public class Main {
 		} catch (InputMismatchException ex) {
 			System.out.println("Por favor ingrese numeros...");
 		}
+	}
+	public static void crearJugador () {
+		Jugador jugador = new Jugador();
+		Calendar fechaNacimiento = Calendar.getInstance();
+		
+		System.out.println("Ingrese el nombre del jugador: ");
+		jugador.setNombre(scanner.nextLine());
+		
+		System.out.println("Ingrese el apellido del jugador: ");
+		jugador.setApellido(scanner.nextLine());
+		
+		System.out.println("Ingrese la fecha de nacimiento del jugador (dd/mm/yyyy): ");
+		String fechaNacimientoStr = scanner.nextLine();
+        String[] fechaParts = fechaNacimientoStr.split("/");
+        int day = Integer.parseInt(fechaParts[0]);
+        int month = Integer.parseInt(fechaParts[1]) - 1;
+        int year = Integer.parseInt(fechaParts[2]);
+        fechaNacimiento.set(year, month, day);
+        jugador.setFechaDeNacimiento(fechaNacimiento);
+        
+        System.out.println("Ingrese la nacionalidad del jugador: ");
+        jugador.setNacionalidad(scanner.nextLine());
+        
+        System.out.println("Ingrese la estatura (cm) del jugador: ");
+        jugador.setEstatura(scanner.nextInt());
+        
+        System.out.println("Ingrese el peso del jugador: ");
+        jugador.setPeso(scanner.nextDouble());
+        
+        Posicion poscicion = validarPosicion();
+        jugador.setPosicion(poscicion);
+        
+        jugadores.add(jugador);
+        
+        System.out.println("Se agregó un nuevo jugador...");
+	}
+	public static Posicion validarPosicion () {
+		int opcion = 0;
+		do {
+			System.out.println("Ingrese la posicion del jugador: ");
+			System.out.println("---- Posicion ----");
+			System.out.println("1 - Delantero");
+			System.out.println("2 - Medio");
+			System.out.println("3 - Defensa");
+			System.out.println("4 - Arquero");
+			System.out.println("Elija una opcion: ");
+			
+			if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+                if (opcion < 1 || opcion > 4) {
+                    System.out.println("Opción no válida. Intente nuevamente...");
+                }
+            } else {
+                scanner.nextLine();
+                System.out.println("Opción no válida. Intente nuevamente...");
+            }
+		} while(opcion < 1 || opcion > 4);
+		
+		Posicion posicion = 
+				Posicion.values()[opcion - 1];
+		return posicion;
 	}
 }
