d7e5692623b880d09c5dc26cec870efd88da09cf
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-24 21:21:23 -0300

    creacion de paquetes y clase enum para el ejercicio3

---
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio3.constantes;
+
+public enum Provincia {
+	JUJUY, 
+	SALTA,
+	TUCUMAN, 
+	CATAMARCA, 
+	LA_RIOJA, 
+	SANTIAGO_DEL_ESTERO;
+}
---
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.ejercicio3.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
