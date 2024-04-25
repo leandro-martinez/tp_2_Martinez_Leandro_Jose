9fa3e80c3655c549120cf266d9c3f708f4ab550b
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-25 16:37:49 -0300

    creacion de clase jugador y enum posicion

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/constantes/Posicion.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/constantes/Posicion.java
@@ -1,5 +1,8 @@
 package ar.edu.unju.fi.ejercicio4.constantes;
 
 public enum Posicion {
-
+	DELANTERO, 
+	MEDIO,
+	DEFENSA,
+	ARQUERO;
 }
--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -1,5 +1,93 @@
 package ar.edu.unju.fi.ejercicio4.model;
 
-public class Jugador {
+import java.util.Calendar;
 
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
+
+public class Jugador {
+	private String nombre;
+	private String apellido;
+	private Calendar fechaDeNacimiento;
+	private String nacionalidad;
+	private int estatura;
+	private double peso;
+	private Posicion posicion;
+	
+	public Jugador () {}
+
+	public Jugador(String nombre, String apellido, Calendar fechaDeNacimiento, String nacionalidad, int estatura,
+			double peso, Posicion posicion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", apellido=" + apellido + ", fechaDeNacimiento=" + fechaDeNacimiento
+				+ ", nacionalidad=" + nacionalidad + ", estatura=" + estatura + ", peso=" + peso + ", posicion="
+				+ posicion + "]";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Calendar getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+
+	public void setFechaDeNacimiento(Calendar fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public int getEstatura() {
+		return estatura;
+	}
+
+	public void setEstatura(int estatura) {
+		this.estatura = estatura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
 }
