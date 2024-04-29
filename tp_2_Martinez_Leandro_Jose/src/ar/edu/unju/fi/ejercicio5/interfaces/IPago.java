ca70a43ad4ba880360934548f0debd2e4c014e3b
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-29 19:08:19 -0300

    interface Pago: metodos añadidos

---
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/interfaces/IPago.java
@@ -0,0 +1,6 @@
+package ar.edu.unju.fi.ejercicio5.interfaces;
+
+public interface IPago {
+	public double realizarPago(double monto);
+	public void imprimirRecibo();
+}
--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/interfaces/Pago.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unju.fi.ejercicio5.interfaces;
-
-public interface Pago {
-
-}
