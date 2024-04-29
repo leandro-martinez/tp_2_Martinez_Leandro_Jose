a834d8975e5b0218254d89e346d5b5443704f40e
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-29 19:56:37 -0300

    creacion del listado de opciones y menu interactivo

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/main/Main.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/main/Main.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.ejercicio5.main;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,32 @@ public class Main {
 	public static void main(String[] args) {
 		productos = new ArrayList<>();
 		scanner = new Scanner(System.in);
-
+		int opcion = 0;
+		
+		try {
+			do {
+				System.out.println("1 - Mostrar productos");
+				System.out.println("2 - Realizar compra");
+				System.out.println("3 - Salir");
+				
+				opcion = scanner.nextInt();
+				scanner.nextLine();
+				
+				switch(opcion) {
+					case 1: System.out.println("Opcion 1");
+						break;
+					case 2: System.out.println("Opcion 2");
+						break;
+					case 3: System.out.println("Fin del programa...");
+						break;
+					default: System.out.println("Opcion no válida");
+						break;
+				}
+				
+			} while(opcion != 3);
+		} catch (InputMismatchException ex) {
+			System.out.println("Por favor ingrese números.");
+		}
 	}
 
 }
