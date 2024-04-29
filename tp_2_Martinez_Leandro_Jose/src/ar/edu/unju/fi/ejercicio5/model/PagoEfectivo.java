38656c78e107031178a474d844492e79c7b3a389
leandro-martinez <leandromartinez022704@gmail.com>
2024-04-29 19:16:45 -0300

    clases: creacion de atributos, constructores, getters y setters

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,5 +1,31 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
-public class PagoEfectivo {
+import java.time.LocalDate;
 
+public class PagoEfectivo {
+	private LocalDate fechaPago;
+	private Double montoPagado;
+	
+	public PagoEfectivo () {}
+
+	public PagoEfectivo(LocalDate fechaPago, Double montoPagado) {
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public Double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(Double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
 }
--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -1,5 +1,41 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
-public class PagoTarjeta {
+import java.time.LocalDate;
 
+public class PagoTarjeta {
+	private String numeroTarjeta;
+	private LocalDate fechaPago;
+	private Double montoPagado;
+	
+	public PagoTarjeta () {}
+
+	public PagoTarjeta(String numeroTarjeta, LocalDate fechaPago, Double montoPagado) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public Double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(Double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
 }
