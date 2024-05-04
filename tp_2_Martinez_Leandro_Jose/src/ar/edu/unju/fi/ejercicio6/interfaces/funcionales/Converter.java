160462ef36ca2b2a58b05121432768d88190146f
leandro-martinez <leandromartinez022704@gmail.com>
2024-05-04 15:01:09 -0300

    interface Converter añadida

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/interfaces/funcionales/Converter.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/interfaces/funcionales/Converter.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.ejercicio6.interfaces.funcionales;
 
-public class Converter {
-
+@FunctionalInterface
+public interface Converter<T, T1> {
+	T1 convert(T t);
+	
+	static <T> boolean isNotNull(T t) {
+		return t != null;
+	}
+	
+	default void mostrarObjeto(T1 t1) {
+		System.out.println("Objeto - " + t1.toString());
+	}
 }
