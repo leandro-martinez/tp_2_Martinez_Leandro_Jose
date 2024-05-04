3d5fd5a0b10ce86df4a60cc7b04fbaf3deb03f85
leandro-martinez <leandromartinez022704@gmail.com>
2024-05-04 14:58:05 -0300

    clases: atributos, constructores, getters and setters añadidos

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/model/FelinoDomestico.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/model/FelinoDomestico.java
@@ -1,5 +1,44 @@
 package ar.edu.unju.fi.ejercicio6.model;
 
 public class FelinoDomestico {
+	private String nombre;
+	private Byte edad;
+	private Float peso;
+	
+	public FelinoDomestico () {}
 
+	@Override
+	public String toString() {
+		return "FelinoDomestico [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + "]";
+	}
+
+	public FelinoDomestico(String nombre, Byte edad, Float peso) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Byte getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Byte edad) {
+		this.edad = edad;
+	}
+
+	public Float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
 }
