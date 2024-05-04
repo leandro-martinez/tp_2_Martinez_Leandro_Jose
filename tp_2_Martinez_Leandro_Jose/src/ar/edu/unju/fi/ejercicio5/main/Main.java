0d41b6b4ebe738ab5bac69fc49267a9a21726b04
leandro-martinez <leandromartinez022704@gmail.com>
2024-05-04 14:06:52 -0300

    funcion realizarCompra: setear estado del producto añadido al carrito

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/main/Main.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio5/main/Main.java
@@ -86,6 +86,7 @@ public class Main {
             if (producto.getCodigo().equalsIgnoreCase(codigoCompra) && producto.getEstado() == true) {
             	productFounded = true;
             	carrito.add(producto);
+            	producto.setEstado(false);
                 System.out.println("Producto agregado al carrito...");
             }
         }
