14775a3347dddb2b3e06c1845ad5d15b98a8f6f5
leandro-martinez <leandromartinez022704@gmail.com>
2024-05-04 15:39:08 -0300

    conversion de felinoSalvaje a felinoDomestico

--- tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp_2_Martinez_Leandro_Jose/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,10 +1,28 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import ar.edu.unju.fi.ejercicio6.interfaces.funcionales.Converter;
+import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+import ar.edu.unju.fi.ejercicio6.model.FelinoSalvaje;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FelinoSalvaje felino = new FelinoSalvaje("Tanner", (byte)20, 186f);
+		//caso de felino = null
+		//felino = null;
+		//definición de expresión lambda que define el convertidor de FelinoSalvaje a
+		//FelinoDomestico.
+		Converter<FelinoSalvaje, FelinoDomestico> converter = 
+				x -> new FelinoDomestico(x.getNombre(), x.getEdad(), x.getPeso());
+		
+		// Verificamos que el objeto felino no sea nulo antes de realizar la conversión
+		if (Converter.isNotNull(felino)) {
+			// Realizamos la conversión
+		    FelinoDomestico domestico = converter.convert(felino);
+		    // Mostramos los datos del objeto felino doméstico
+		    converter.mostrarObjeto(domestico);
+		} else {
+			System.out.println("El objeto felino es nulo.");
+		}
 	}
 
 }
